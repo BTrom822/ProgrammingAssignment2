672152d388550becf01e8ca7538e6f618f3a59b3
BTrom822 <briantrommater@gmail.com>
2017-08-30 15:25:35 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 2 functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Create a matrix object that can cache its inverse
+makeCacheMatrix <- function( m = matrix() ) {
+        
+        ## set the inverse property
+        v <- NULL
+        
+        ## set the matrix
+        set <- function( matrix ) {
+                m <<- matrix
+                v <<- NULL
+        }
+        
+        ## get the matrix
+        get <- function() {
+                ## Return the matrix
+                m
+        }
+        
+        ## set the inverse of the matrix
+        setIV <- function(inverse) {
+                v <<- inverse
+        }
+        
+        ## get the inverse of the matrix
+        getIV <- function() {
+                ## Return the inverse property
+                v
+        }
+        
+        ## Return a list of the methods
+        list(set = set, get = get,
+             setIV = setIV,
+             getIV = getIV)
 }
 
 
-## Write a short comment describing this function
-
+## Calculates inverse of the matrix returned by "makeCacheMatrix"
+## If inverse has already been calculated and no changes since,
+## then the "cachesolve" retrieves the inverse from the cache.
 cacheSolve <- function(x, ...) {
+        
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getIV()
+        
+        ## Just return the inverse if its already set
+        if( !is.null(m) ) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        ## Get the matrix from our object
+        data <- x$get()
+        
+        ## Calculate the inverse using matrix multiplication
+        m <- solve(data) %*% data
+        
+        ## Set the inverse to the object
+        x$setIV(m)
+        
+        ## Return the matrix
+        m
 }
